1c0e74bf785b95a297a752655cc4ef61dc293a68
brianuribe6 <brian.uribe@outlook.com>
2023-01-17 22:52:55 -0500

    WIP Collaboration test

--- components/Paper/index.tsx
+++ components/Paper/index.tsx
@@ -13,9 +13,36 @@ import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPl
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
+import {
+  $createParagraphNode,
+  $createTextNode,
+  $getRoot,
+  LexicalEditor,
+} from "lexical";
 import theme from "./theme";
 
+import dynamic from "next/dynamic";
+import { WebsocketProvider } from "y-websocket";
+import * as Y from "yjs";
+
+const CollaborationPlugin = dynamic(
+  () =>
+    import("@lexical/react/LexicalCollaborationPlugin").then(
+      (module) => module.CollaborationPlugin
+    ),
+  { ssr: false }
+);
+
+function initialEditorState(editor: LexicalEditor): void {
+  const root = $getRoot();
+  const paragraph = $createParagraphNode();
+  const text = $createTextNode("Welcome to collab!");
+  paragraph.append(text);
+  root.append(paragraph);
+}
+
 const editorConfig: InitialConfigType = {
+  editorState: null,
   namespace: "My Editor",
   theme,
   onError(error, editor) {
@@ -48,6 +75,27 @@ function Paper() {
           />
           <AutoFocusPlugin />
           <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+          <CollaborationPlugin
+            id="yjs-plugin"
+            providerFactory={(id, yjsDocMap) => {
+              const doc = new Y.Doc();
+              yjsDocMap.set(id, doc);
+
+              const provider = new WebsocketProvider(
+                "ws://localhost:1234",
+                id,
+                doc
+              );
+
+              return provider;
+            }}
+            // Optional initial editor state in case collaborative Y.Doc won't
+            // have any existing data on server. Then it'll user this value to populate editor.
+            // It accepts same type of values as LexicalComposer editorState
+            // prop (json string, state object, or a function)
+            initialEditorState={initialEditorState}
+            shouldBootstrap={true}
+          />
         </LexicalComposer>
       </div>
     </div>
