85fe6ce8e38aff6355d0d56edc54cbf70f91507d
brianuribe6 <brian.uribe@outlook.com>
2023-01-28 12:48:19 -0400

    Add Polymorphic 'as' prop type utlities

---
+++ components/helpers.d.ts
@@ -0,0 +1,26 @@
+import React from "react";
+
+export type ReplaceProps<Inner extends React.ElementType, P> = Omit<
+    React.ComponentPropsWithRef<Inner>,
+    P
+> &
+    P;
+
+export interface AsProp<As extends React.ElementType = React.ElementType> {
+    as?: As;
+}
+
+export interface RefForwardingComponent<
+    TInitial extends React.ElementType,
+    TProps = unknown,
+> {
+    <As extends React.ElementType = TInitial>(
+        props: React.PropsWithChildren<ReplaceProps<As, AsProp<As> & TProps>>,
+        // context?: any,
+    ): React.ReactElement | null;
+    // propTypes?: any;
+    // contextTypes?: any;
+    // defaultProps?: Partial<TProps>;
+    displayName?: string;
+}
+
