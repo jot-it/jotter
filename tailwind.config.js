15d1a14f3f5e7e747dde88faffa27cf7659d2268
brianuribe6 <brian.uribe@outlook.com>
2023-05-06 16:26:30 -0400

    Move code to src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     fontFamily: {
