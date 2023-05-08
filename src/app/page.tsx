fc7d52c6d7c6b8519ed5416fed82011e19a37821
Brian Uribe <45882589+BrianUribe6@users.noreply.github.com>
2023-05-08 17:48:26 -0400

    Separate server and client components (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,21 @@
-"use client";
-import Link from "next/link";
-import { useState } from "react";
-import { RiBook2Line as BookIcon } from "react-icons/ri";
-import Avatar from "../components/Avatar";
-import Breadcrumbs from "../components/Breadcrumbs";
 import Paper from "../components/Paper";
-import Sidebar from "../components/Sidebar";
-import Typography from "../components/Typography";
-import sidebarConfig from "../components/sidebarConfig";
-// import { Metadata } from "next";
+import Header from "./Header";
+import SideNavigation from "./SideNavigation";
 
-// export const metadata: Metadata = {
-//   title: "MD editor",
-//   description: "Markdown editor",
-// };
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MD editor",
+  description: "Markdown editor",
+};
 
 export default function Home() {
-  const [sidebar, setSidebar] = useState(sidebarConfig);
-
   return (
     <>
       <div className="grid min-h-screen grid-cols-[1fr_3fr]">
-        <Sidebar>
-          {/* Add new page */}
-          <Sidebar.ItemList items={sidebar} />
-          <div className="space-y-1">
-            <Sidebar.Divider />
-            <Sidebar.AddButton
-              onClick={() =>
-                setSidebar([
-                  ...sidebar,
-                  {
-                    type: "link",
-                    label: "New page",
-                    href: "#new-page",
-                  },
-                ])
-              }
-            />
-          </div>
-        </Sidebar>
+        <SideNavigation />
         <main>
-          <header className="flex items-center border-b border-gray-200 p-4 shadow-sm">
-            <Breadcrumbs aria-label="Breadcrumb">
-              <Link href="#">
-                <Typography as="span" variant="body1">
-                  <BookIcon className="mr-1 inline-block text-lg" />
-                  Chapter 1
-                </Typography>
-              </Link>
-              <Link href="#">Section a</Link>
-              <Link href="#">Topic 1</Link>
-            </Breadcrumbs>
-
-            {/* Current users in this notebook */}
-            <div className="ml-auto mr-4 flex -space-x-1 text-white">
-              <Avatar className="bg-gray-200 text-gray-700" size="lg">
-                +5
-              </Avatar>
-              <Avatar className="bg-orange-500 ring ring-white" size="lg">
-                JD
-              </Avatar>
-              <Avatar className="bg-red-500 ring ring-white" size="lg">
-                KS
-              </Avatar>
-              <Avatar className="bg-indigo-500 ring ring-white" size="lg">
-                MG
-              </Avatar>
-            </div>
-
-            <Typography
-              as="button"
-              variant="button"
-              className="rounded-md bg-gray-700 px-4 py-2 text-white shadow-sm hover:bg-gray-600"
-            >
-              Share
-            </Typography>
-          </header>
+          <Header />
           <Paper />
         </main>
       </div>
