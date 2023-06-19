d23ed0dcf4cbc20da9b72e3e9761765466138a27
brianuribe6 <brian.uribe@outlook.com>
2023-06-19 01:30:20 -0400

    Rename useOutsideClick, and refactor callbackRef

---
+++ src/hooks/useOnOutsideClick.ts
@@ -0,0 +1,28 @@
+import { RefObject, useEffect } from "react";
+import { useCallbackRef } from "./useCallbackRef";
+
+type ClickHandler = (e: MouseEvent) => void;
+
+export const useOnOutsideClick = (
+  ref: RefObject<HTMLElement>,
+  onClick: ClickHandler
+) => {
+  const onClickCallback = useCallbackRef(onClick);
+
+  useEffect(() => {
+    const handleClick: ClickHandler = (e) => {
+      if (!(e.target instanceof HTMLElement)) {
+        return;
+      }
+
+      if (ref.current && !ref.current.contains(e.target)) {
+        onClickCallback(e);
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [ref, onClickCallback]);
+};
