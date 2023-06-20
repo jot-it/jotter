c0c5b65deeba1cdacc9e77029a3e729e2532e182
Brian Uribe <45882589+BrianUribe6@users.noreply.github.com>
2023-06-20 15:53:56 -0400

    Code cleanup (#20)
    * Add useCallbackRef hook
    
    * Rename useOutsideClick, and refactor callbackRef
    
    * Extract Menu Options into their own components

--- src/app/SideNavigation.tsx
+++ src/app/SideNavigation.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { RiAddLine as AddIcon } from "react-icons/ri";
-import ContextMenu from "../components/ContextMenu";
 import Sidebar from "../components/Sidebar";
 import { useSidebarDispatch } from "../components/Sidebar/context";
-import { SidebarOption } from "../components/Sidebar/optionsContexMenu";
 import sidebarConfig from "../sidebarConfig";
 
 function SideNavigation() {
@@ -18,25 +16,19 @@ function SidebarContent() {
   const dispatch = useSidebarDispatch();
   return (
     <>
-      <div className="flex-1 overflow-auto">
-        <ContextMenu>
-          <Sidebar.Items />
-          <SidebarOption />
-        </ContextMenu>
-      </div>
-
+      <Sidebar.Items />
       <div className="space-y-1">
         <Sidebar.Divider />
         <Sidebar.Button
           onClick={() =>
             dispatch({
               type: "create",
-              newItem :{
+              newItem: {
                 label: "new item",
                 id: crypto.randomUUID(),
                 href: "#ni001",
                 type: "link",
-                isEditing : true,
+                isEditing: true,
               },
             })
           }
--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,65 +1,61 @@
-import * as Contextmenu from "@radix-ui/react-context-menu";
+import * as ContextMenuPrimitive from "@radix-ui/react-context-menu";
 import clsx from "clsx";
-import { PropsWithChildren, forwardRef } from "react";
+import { PropsWithChildren, ReactNode, forwardRef } from "react";
 
-function ContextMenu({ children }: PropsWithChildren) {
+type ContextMenuProps = PropsWithChildren<{
+  /** Element that will have contextual menu behaviour. */
+  trigger: ReactNode;
+}>;
+
+function ContextMenu({ children, trigger }: ContextMenuProps) {
   return (
-    <Contextmenu.Root>
-      <Trigger>{children}</Trigger>
-    </Contextmenu.Root>
+    <ContextMenuPrimitive.Root>
+      <ContextMenuPrimitive.Trigger>{trigger}</ContextMenuPrimitive.Trigger>
+      {children}
+    </ContextMenuPrimitive.Root>
   );
 }
 
-const Trigger = forwardRef<
-  HTMLSpanElement,
-  Contextmenu.ContextMenuTriggerProps
->(function Trigger({ children, ...props }, ref) {
-  return (
-    <Contextmenu.Trigger ref={ref} {...props}>
-      {children}
-    </Contextmenu.Trigger>
-  );
-});
-
-const Option = forwardRef<HTMLDivElement, Contextmenu.MenuItemProps>(
+const Option = forwardRef<HTMLDivElement, ContextMenuPrimitive.MenuItemProps>(
   function Option({ children, className, ...props }, ref) {
     return (
-      <Contextmenu.Item
+      <ContextMenuPrimitive.Item
         ref={ref}
         {...props}
         className={clsx(
           className,
-          " flex select-none items-center rounded-md px-2 py-1 data-[highlighted]:bg-gray-200/75 dark:data-[highlighted]:bg-slate-800 dark:data-[highlighted]:text-cyan-300"
+          "flex select-none items-center rounded-md px-2 py-1 data-[highlighted]:bg-gray-200/75",
+          "dark:data-[highlighted]:bg-slate-800 dark:data-[highlighted]:text-cyan-300"
         )}
       >
         {children}
-      </Contextmenu.Item>
+      </ContextMenuPrimitive.Item>
     );
   }
 );
 
 function Divider() {
   return (
-    <Contextmenu.Separator className="m-1 h-0.5 bg-gray-200 dark:bg-slate-600" />
+    <ContextMenuPrimitive.Separator className="m-1 h-0.5 bg-gray-200 dark:bg-slate-600" />
   );
 }
 
-const Content = forwardRef<HTMLDivElement, Contextmenu.MenuContentProps>(
-  function Body({ children, ...props }, ref) {
-    return (
-      <Contextmenu.Content
-        className="min-w-[220px] overflow-hidden rounded-lg bg-gray-50  p-2 drop-shadow-md data-[disabled]:text-gray-500 dark:bg-slate-700"
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </Contextmenu.Content>
-    );
-  }
-);
+const Content = forwardRef<
+  HTMLDivElement,
+  ContextMenuPrimitive.MenuContentProps
+>(function Body({ children, ...props }, ref) {
+  return (
+    <ContextMenuPrimitive.Content
+      className="min-w-[220px] overflow-hidden rounded-lg bg-gray-50  p-2 drop-shadow-md data-[disabled]:text-gray-500 dark:bg-slate-700"
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </ContextMenuPrimitive.Content>
+  );
+});
 
 export default Object.assign(ContextMenu, {
-  Trigger,
   Option,
   Content,
   Divider,
