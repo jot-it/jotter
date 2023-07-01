23cde934d6ee5b0fb37092ca1d6f871c157df65d
brianuribe6 <brian.uribe@outlook.com>
2023-07-01 00:30:33 -0400

    Install radix dialog

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,32 @@ module.exports = {
           850: "#191f2f",
         },
       },
+      keyframes: {
+        "fade-in": {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        "fade-out": {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+        "slide-in": {
+          "0%": {
+            translate: "-100%",
+          },
+        },
+        "slide-out": {
+          "100%": {
+            translate: "-100%",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 200ms ease-in-out",
+        "fade-out": "fade-out 200ms ease-in-out",
+        "slide-in": "slide-in 200ms ease-in-out",
+        "slide-out": "slide-out 200ms ease-in-out",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
