4eebf3445a47c9522eda7e1941739a7a1ff72b69
Luis Jose Nivar <luisjosenivarn@hotmail.com>
2023-08-01 18:23:50 -0400

    Open new tab with click on link

--- src/plugins/AutolinkPlugin/index.tsx
+++ src/plugins/AutolinkPlugin/index.tsx
@@ -1,32 +1,44 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 import {
   AutoLinkPlugin,
   createLinkMatcherWithRegExp,
 } from "@lexical/react/LexicalAutoLinkPlugin";
 import * as React from "react";
+import { NodeEventPlugin } from "@lexical/react/LexicalNodeEventPlugin";
+import { AutoLinkNode, LinkNode } from "@lexical/link";
 
 const URL_REGEX =
   /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 const EMAIL_REGEX =
-  /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 const MATCHERS = [
   createLinkMatcherWithRegExp(URL_REGEX, (text) => {
     return text.startsWith("http") ? text : `https://${text}`;
   }),
+
   createLinkMatcherWithRegExp(EMAIL_REGEX, (text) => {
     return `mailto:${text}`;
   }),
 ];
 
-export default function LexicalAutoLinkPlugin(): JSX.Element {
-  return <AutoLinkPlugin matchers={MATCHERS} />;
+export default function LexicalAutoLinkPlugin() {
+  return (
+    <>
+      <NodeEventPlugin
+        nodeType={AutoLinkNode}
+        eventType={"click"}
+        eventListener={(e: Event) => {
+          const node = e.target as HTMLLinkElement;
+          const url = node.parentElement?.getAttribute("href");
+
+          if (url) {
+            window.open(url, "_blank", "noreferrer");
+          }
+        }}
+      />
+
+      <AutoLinkPlugin matchers={MATCHERS} />
+    </>
+  );
 }
