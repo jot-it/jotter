1d00c4ad15c297300161b2491e26617317cdcc58
brianuribe6 <brian.uribe@outlook.com>
2023-11-13 14:48:50 -0500

    Allow list indentation with tab key press

--- src/app/note/[id]/theme.tsx
+++ src/app/note/[id]/theme.tsx
@@ -6,6 +6,11 @@ const theme: EditorThemeClasses = {
     underline: "underline",
     underlineStrikethrough: "underline-through",
   },
+  list: {
+    nested: {
+      listitem: "list-none"
+    }
+  },
   link: "dark:text-cyan-400 dark:visited:text-indigo-400 text-cyan-600 cursor-pointer",
 };
 
