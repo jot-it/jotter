15b691aebd0e2d3b830f64ebe78adcb7b6ea477e
brianuribe6 <brian.uribe@outlook.com>
2023-11-28 20:54:11 -0500

    Added TailwindCSS intellisense support for cva

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,21 +3,21 @@ import { VariantProps, cva } from "class-variance-authority";
 import { ComponentPropsWithoutRef, forwardRef } from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center gap-1 md:rounded-md shadow-sm md:px-4 md:py-2 whitespace-nowrap",
+  "inline-flex items-center gap-1 whitespace-nowrap shadow-sm md:rounded-md md:px-4 md:py-2",
   {
     variants: {
       variant: {
         primary:
           "bg-cyan-200 text-cyan-900 hover:bg-cyan-300 dark:bg-cyan-900 dark:text-cyan-200 dark:hover:bg-cyan-800",
         danger:
-          "outline outline-1 -outline-offset-1 outline-red-200 text-cyan-900  outline-red-400 text-red-400 hover:outline-red-300 dark:outline-red-400 dark:text-red-400  dark:hover:text-red-300 dark:hover:outline-red-300",
+          "text-cyan-900 text-red-400 outline outline-1 -outline-offset-1  outline-red-200 outline-red-400 hover:outline-red-300 dark:text-red-400 dark:outline-red-400  dark:hover:text-red-300 dark:hover:outline-red-300",
         secondary:
-          "outline outline-1 -outline-offset-1 outline-cyan-200 text-cyan-900 hover:dark:text-cyan-200 hover:outline-cyan-200 dark:outline-cyan-400 dark:text-cyan-400 dark:hover:outline-cyan-200",
+          "text-cyan-900 outline outline-1 -outline-offset-1 outline-cyan-200 hover:outline-cyan-200 dark:text-cyan-400 dark:outline-cyan-400 hover:dark:text-cyan-200 dark:hover:outline-cyan-200",
       },
       size: {
         medium: "p3",
-        small: "py-2 px-1 text-sm",
-        large: "py-4 px-5 text-2xl",
+        small: "px-1 py-2 text-sm",
+        large: "px-5 py-4 text-2xl",
       },
     },
     defaultVariants: {
