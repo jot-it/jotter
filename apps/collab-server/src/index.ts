edc71029f8d0c1facbfe86189a538af4d7c65f7b
brianuribe6 <brian.uribe@outlook.com>
2023-12-01 15:40:26 -0500

    Add Sqlite extension

--- apps/collab-server/src/index.ts
+++ apps/collab-server/src/index.ts
@@ -1,10 +1,12 @@
 import { Hocuspocus } from "@hocuspocus/server";
+import { SQLite } from "@hocuspocus/extension-sqlite";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 1234;
 
 // Configure the server …
 const server = new Hocuspocus({
   port: PORT,
+  extensions: [new SQLite()],
 });
 
 // … and run it!
