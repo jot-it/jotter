ec08a2074186c8f5c514a39fbe8941c58301994d
brianuribe6 <brian.uribe@outlook.com>
2023-12-01 16:33:49 -0500

    Add Logger extension

--- apps/collab-server/src/index.ts
+++ apps/collab-server/src/index.ts
@@ -1,12 +1,13 @@
 import { Hocuspocus } from "@hocuspocus/server";
 import { SQLite } from "@hocuspocus/extension-sqlite";
+import { Logger } from "@hocuspocus/extension-logger";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 1234;
 
 // Configure the server …
 const server = new Hocuspocus({
   port: PORT,
-  extensions: [new SQLite()],
+  extensions: [new SQLite(), new Logger()],
 });
 
 // … and run it!
