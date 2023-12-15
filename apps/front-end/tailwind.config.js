04eba05d643464d82a5f1b096eed73d6ecf843ec
brianuribe6 <brian.uribe@outlook.com>
2023-12-15 16:58:15 -0500

    Force darkMode on tailwind config

--- apps/front-end/tailwind.config.js
+++ apps/front-end/tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
