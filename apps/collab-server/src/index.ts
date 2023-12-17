6c7b04bddf0bce9b15b4340aaa6bcf4053c03376
Brian Uribe <45882589+BrianUribe6@users.noreply.github.com>
2023-12-17 16:16:11 -0500

    Add support for persisting documents changes on MySQL (#49)
    * Add Support for persisting documents on a MySQL database
    
    * Add collab-server debug vscode launch config
    
    * Add example env file, and set up Development docker configuration
    
    * Bind mount node_modules folder
    
    * Remove NODE_ENV from package.json since this is incompatible with windows
    
    * Change runner as prod stagee and set NODE env to production during install
    
    * Fix local node_modules leaking into container. Separate dev and prod stages

--- apps/collab-server/src/index.ts
+++ apps/collab-server/src/index.ts
@@ -1,14 +1,20 @@
-import { Hocuspocus } from "@hocuspocus/server";
-import { SQLite } from "@hocuspocus/extension-sqlite";
 import { Logger } from "@hocuspocus/extension-logger";
+import { Hocuspocus } from "@hocuspocus/server";
+import MySQL from "./extensions/MySQL.js";
+// import "./loadEnv.js";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 1234;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL must be defined to persist document changes");
+}
 
 // Configure the server …
 const server = new Hocuspocus({
   port: PORT,
-  extensions: [new SQLite(), new Logger()],
+  extensions: [new Logger(), new MySQL(DATABASE_URL)],
 });
 
-// … and run it!
+// // … and run it!
 server.listen();
