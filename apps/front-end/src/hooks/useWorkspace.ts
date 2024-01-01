28041941696bd43b1c240b21d282261c3fa855df
brianuribe6 <brian.uribe@outlook.com>
2024-01-01 17:53:46 -0500

    Move useEffect to atom's mount

--- apps/front-end/src/hooks/useWorkspace.ts
+++ apps/front-end/src/hooks/useWorkspace.ts
@@ -1,30 +1,24 @@
-import { IS_BROWSER } from "@/utils";
-import { useAtom } from "jotai";
+import { useAtomValue } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { nanoid } from "nanoid";
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
 
-const workspaceAtom = atomWithStorage("workspace", "server");
+const WORKSPACE_KEY = "workspace";
+export const workspaceAtom = atomWithStorage(WORKSPACE_KEY, "");
+
+workspaceAtom.onMount = (set) => {
+  if (!localStorage.getItem(WORKSPACE_KEY)) {
+    set(nanoid());
+  }
+};
 
 export default function useWorkspace() {
   const params = useParams<{ workspace: string; id: string }>();
   const { workspace: urlWorkspace } = params;
-  const [workspace, setWorkspace] = useAtom(workspaceAtom);
-
-  useEffect(() => {
-    if (!localStorage.getItem("workspace")) {
-      setWorkspace(nanoid());
-    }
-  }, []);
-
+  const workspace = useAtomValue(workspaceAtom);
   if (urlWorkspace) {
     return urlWorkspace;
   }
 
   return workspace;
 }
-
-// 1° del URL
-// 2° LocalStorage
-// 3° Lo crea
