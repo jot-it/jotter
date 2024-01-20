66ee970fc56b0ee3b54c4f5d200e83ab6cb16e54
Brian Uribe <45882589+BrianUribe6@users.noreply.github.com>
2024-01-20 11:55:09 -0500

    Fix toolbar appearing on code highlight node (#67)

--- apps/front-end/src/plugins/ToolbarPlugin/index.tsx
+++ apps/front-end/src/plugins/ToolbarPlugin/index.tsx
@@ -1,7 +1,20 @@
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { useEffect, useState } from "react";
-import Toolbar from "./Toolbar";
 import useMatchMedia from "@/hooks/useMatchMedia";
+import { $isCodeHighlightNode } from "@lexical/code";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import {
+  $getSelection,
+  $isParagraphNode,
+  $isRangeSelection,
+  $isTextNode,
+  BaseSelection,
+  COMMAND_PRIORITY_EDITOR,
+  LexicalEditor,
+  SELECTION_CHANGE_COMMAND,
+} from "lexical";
+import { useCallback, useEffect, useState } from "react";
+import useLexicalCommand from "../useLexicalCommand";
+import Toolbar from "./Toolbar";
+import { getSelectedNode } from "./utils";
 
 type ToolbarPluginProps = {
   /**
@@ -14,32 +27,55 @@ type ToolbarPluginProps = {
 
 function ToolbarPlugin(props: ToolbarPluginProps) {
   const [editor] = useLexicalComposerContext();
-  const [editorHasSelection, setEditorHasSelection] = useState(false);  
+  const [show, setShow] = useState(false);
   const isMobileScreen = useMatchMedia("(max-width: 1023px)");
 
+  const updateToolbarVisibility = useCallback(() => {
+    editor.getEditorState().read(() => {
+      const selection = $getSelection();
+      setShow(isSelectionInside(editor) && isTextualSelection(selection));
+    });
+  }, []);
+
+  useLexicalCommand(
+    SELECTION_CHANGE_COMMAND,
+    () => {
+      updateToolbarVisibility();
+      return true;
+    },
+    COMMAND_PRIORITY_EDITOR,
+  );
+
   useEffect(() => {
-    const updateToolbarVisibility = () => {
-      const selection = window.getSelection();
-      const editorRoot = editor.getRootElement();
-
-      const hasSelection = selection && !selection.isCollapsed;
-      const isSelectionInsideEditor =
-        hasSelection && editorRoot && editorRoot.contains(selection.anchorNode);
-
-      setEditorHasSelection(Boolean(isSelectionInsideEditor));
-    };
-
     document.addEventListener("selectionchange", updateToolbarVisibility);
     return () => {
       document.removeEventListener("selectionchange", updateToolbarVisibility);
     };
-  }, [editor]);
+  }, [editor, updateToolbarVisibility]);
 
-  if (!editorHasSelection && !isMobileScreen) {
+  if (!show && !isMobileScreen) {
     return null;
   }
 
-  return <Toolbar {...props} editor={editor} fixedBottom={isMobileScreen}/>;
+  return <Toolbar {...props} editor={editor} fixedBottom={isMobileScreen} />;
+}
+
+function isSelectionInside(editor: LexicalEditor) {
+  const selection = window.getSelection();
+  const editorRoot = editor.getRootElement();
+  const hasSelection = selection && !selection.isCollapsed;
+  return Boolean(hasSelection && editorRoot?.contains(selection.anchorNode));
+}
+
+function isTextualSelection(selection: BaseSelection | null): boolean {
+  if (!$isRangeSelection(selection)) {
+    return false;
+  }
+  const selectedNode = getSelectedNode(selection);
+  if ($isCodeHighlightNode(selectedNode)) {
+    return false;
+  }
+  return $isParagraphNode(selectedNode) || $isTextNode(selectedNode);
 }
 
 export default ToolbarPlugin;
