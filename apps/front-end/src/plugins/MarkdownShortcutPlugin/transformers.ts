de08694278afa05c345bf8809d4088cf6783f891
Luis Jose Nivar <luisjosenivarn@hotmail.com>
2024-01-23 20:16:14 -0400

    Add CheckListTransformer

--- apps/front-end/src/plugins/MarkdownShortcutPlugin/transformers.ts
+++ apps/front-end/src/plugins/MarkdownShortcutPlugin/transformers.ts
@@ -1,11 +1,13 @@
 import {
+  CHECK_LIST,
+  ElementTransformer,
   TRANSFORMERS as LEXICAL_TRANSFORMERS,
   TextMatchTransformer,
 } from "@lexical/markdown";
 import {
-  ImageNode,
-  $isImageNode,
   $createImageNode,
+  $isImageNode,
+  ImageNode,
 } from "../ImagePlugin/ImageNode";
 
 const IMAGE_TRANFORMER: TextMatchTransformer = {
@@ -31,9 +33,13 @@ const IMAGE_TRANFORMER: TextMatchTransformer = {
   },
 };
 
+const CHECK_LIST_TRANSFORMER: ElementTransformer = CHECK_LIST;
+CHECK_LIST_TRANSFORMER.regExp = /^(\s*)(?:-)?\s?(\[(\s|x)?\])\s/i;
+
 // Register all transformers here
 const MARKDOWN_TRANFORMS: typeof LEXICAL_TRANSFORMERS = [
   IMAGE_TRANFORMER,
+  CHECK_LIST_TRANSFORMER,
   ...LEXICAL_TRANSFORMERS,
 ];
 
