281f59472dcac606a1762cdabf6e9238133d9fc9
brianuribe6 <brian.uribe@outlook.com>
2024-01-28 18:46:54 -0500

    Add dropdown menu component

--- apps/front-end/src/app/[workspace]/HomeScreenActions.tsx
+++ apps/front-end/src/app/[workspace]/HomeScreenActions.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Button from "@/components/Button";
+import { DropdownMenuDemo } from "./TestDropdown";
 import { BookIcon, FileIcon } from "@/components/Icons";
 import { newCategory, newPage, sidebarState } from "@/components/Sidebar/state";
 import useWorkspace from "@/hooks/useWorkspace";
@@ -22,6 +23,10 @@ function HomeScreenActions() {
           <BookIcon className="mr-2" />
           New Category
         </Button>
+      </li>
+
+      <li>
+        <DropdownMenuDemo />
       </li>
     </ul>
   );
