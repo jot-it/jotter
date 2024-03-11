7f5ca7e565c9685fcca0f89ecbd774034c1c8fb2
brianuribe6 <brian.uribe@outlook.com>
2024-03-11 03:36:21 -0400

    Migrate collab server to Turso

--- apps/collab-server/src/index.ts
+++ apps/collab-server/src/index.ts
@@ -1,11 +1,11 @@
+import { Database } from "@hocuspocus/extension-database";
 import { Logger } from "@hocuspocus/extension-logger";
 import { Hocuspocus } from "@hocuspocus/server";
-import MySQL from "./extensions/MySQL.js";
-import { env } from "./env.js";
 import { jwtVerify } from "jose";
+import { env } from "./env.js";
+import tursoConfiguration from "./turso-config.js";
 
 const PORT = env.PORT ? Number(env.PORT) : 1234;
-const IS_PRODUCTION = env.NODE_ENV === "production";
 
 // Configure the server …
 const server = new Hocuspocus({
@@ -16,22 +16,7 @@ const server = new Hocuspocus({
     await jwtVerify(data.token, key);
   },
 
-  extensions: [
-    new Logger(),
-
-    new MySQL({
-      user: env.DATABASE_USER,
-      host: env.DATABASE_HOST,
-      database: env.DATABASE_NAME,
-      password: env.DATABASE_PASS,
-
-      // Enable SSL on production. Production database will reject the connection
-      // when this flag is not set.
-      ssl: {
-        rejectUnauthorized: IS_PRODUCTION,
-      },
-    }),
-  ],
+  extensions: [new Logger(), new Database(tursoConfiguration)],
 });
 
 // // … and run it!
