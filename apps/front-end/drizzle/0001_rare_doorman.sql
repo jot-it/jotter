0f6637ed5fa218f69e027a3d0f1f65d71d15a744
Brian Uribe <45882589+BrianUribe6@users.noreply.github.com>
2024-03-14 19:03:42 -0400

    Update migrations (#77)
    * Generate new initial migrations
    
    * Add migrate, migrate-prod command

--- apps/front-end/drizzle/0001_rare_doorman.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `account` DROP FOREIGN KEY `account_userId_user_id_fk`;
---> statement-breakpoint
-ALTER TABLE `notebook` DROP FOREIGN KEY `notebook_documentName_document_name_fk`;
---> statement-breakpoint
-ALTER TABLE `notebook` DROP FOREIGN KEY `notebook_authorId_user_id_fk`;
---> statement-breakpoint
-ALTER TABLE `session` DROP FOREIGN KEY `session_userId_user_id_fk`;
--- apps/front-end/src/lib/db.ts
+++ apps/front-end/src/lib/db.ts
@@ -1,11 +1,10 @@
-import env from "@/config/env-server";
 import * as schema from "@/schema";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
-const connection = await createClient({
-  url: env.TURSO_DATABASE_URL,
-  authToken: env.TURSO_AUTH_TOKEN,
+export const connection = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 const db = drizzle(connection, { schema });
